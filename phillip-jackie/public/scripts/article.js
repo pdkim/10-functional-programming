a1d0849416f5598a099d13434456729ae70f8d34
Phillip Kim <phildkim@gmail.com>
2018-04-11 21:18:27 -0700

    we are so lost

--- phillip-jackie/public/scripts/article.js
+++ phillip-jackie/public/scripts/article.js
@@ -1,5 +1,6 @@
 'use strict';
 var app = app || {};
+
 (function (module){
   function Article(rawDataObj) {
   // REVIEW: In Lab 8, we explored a lot of new functionality going on here. Let's re-examine the concept of context. Normally, "this" inside of a constructor function refers to the newly instantiated object. However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object. One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context. That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function. As a result, we no longer have to pass in the optional "this" argument to forEach!
@@ -94,4 +95,6 @@ var app = app || {};
       .then(console.log)
       .then(callback);
   };
-}());
+
+  module.article = Article
+}(app));
